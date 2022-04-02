9d4e1e7ca87ba3a8a2b27facfff35ddfafd9d963
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-02 10:38:31 -0300

    Add news model

--- src/main/java/com/claudionogueira/news/models/Author.java
+++ src/main/java/com/claudionogueira/news/models/Author.java
@@ -1,13 +1,18 @@
 package com.claudionogueira.news.models;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "TB_AUTHOR")
@@ -26,6 +31,10 @@ public class Author implements Serializable {
 
 	@Column(name = "email")
 	private String email;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "author")
+	private Set<News> authorNews = new HashSet<>();
 
 	public Author() {
 
---
+++ src/main/java/com/claudionogueira/news/models/News.java
@@ -0,0 +1,94 @@
+package com.claudionogueira.news.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "TB_NEWS")
+public class News implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Lob
+	@Column(name = "content")
+	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private Author author;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private LocalDate date;
+
+	public News() {
+
+	}
+
+	public News(Long id, String title, String content, Author author, LocalDate date) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+}
