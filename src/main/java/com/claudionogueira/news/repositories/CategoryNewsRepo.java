b3285caf6e75313286c388a48c7da1bf375ad713
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-02 12:59:43 -0300

    Add CategoryNews repository

---
+++ src/main/java/com/claudionogueira/news/repositories/CategoryNewsRepo.java
@@ -0,0 +1,12 @@
+package com.claudionogueira.news.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.claudionogueira.news.models.CategoryNews;
+import com.claudionogueira.news.models.CategoryNewsPK;
+
+@Repository
+public interface CategoryNewsRepo extends JpaRepository<CategoryNews, CategoryNewsPK>{
+
+}
