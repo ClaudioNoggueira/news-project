df5a3a5959e5cf5e2b2bbfd8000129cd8dc33074
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-02 12:29:21 -0300

    Add category mock data

--- src/main/java/com/claudionogueira/news/utils/MockData.java
+++ src/main/java/com/claudionogueira/news/utils/MockData.java
@@ -7,28 +7,47 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.claudionogueira.news.models.Author;
+import com.claudionogueira.news.models.Category;
 import com.claudionogueira.news.repositories.AuthorRepo;
+import com.claudionogueira.news.repositories.CategoryRepo;
 
 @Component
-public class MockData implements CommandLineRunner{
+public class MockData implements CommandLineRunner {
 
 	private final AuthorRepo authorRepo;
 
-	public MockData(AuthorRepo authorRepo) {
+	private final CategoryRepo categoryRepo;
+
+	public MockData(AuthorRepo authorRepo, CategoryRepo categoryRepo) {
 		this.authorRepo = authorRepo;
+		this.categoryRepo = categoryRepo;
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
+		// Save authors
 		List<Author> authors = new ArrayList<>();
 		authors.add(new Author(null, "Elliot", "Cassie", "ecassie0@cdbaby.com"));
 		authors.add(new Author(null, "Mariellen", "Stanbridge", "mstanbridge1@mapy.cz"));
 		authors.add(new Author(null, "Lonnie", "Charopen", "lcharopen2@skyrock.com"));
 		authors.add(new Author(null, "Izzy", "McCorry", "imccorry3@cisco.com"));
 		authors.add(new Author(null, "Meredith", "Lamcken", "mlamcken4@netscape.com"));
-		
-		for(Author author : authors) {
+
+		for (Author author : authors) {
 			authorRepo.save(author);
-		}		
+		}
+
+		// Save categories
+		List<Category> categories = new ArrayList<>();
+		categories.add(new Category(null, "Sports"));
+		categories.add(new Category(null, "Politics"));
+		categories.add(new Category(null, "Cinema"));
+		categories.add(new Category(null, "Economy"));
+		categories.add(new Category(null, "Science"));
+
+		for (Category category : categories) {
+			categoryRepo.save(category);
+		}
+
 	}
 }
