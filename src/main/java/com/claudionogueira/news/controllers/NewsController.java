08c5d839afa07ea0b90cfeedeb0bc2431d296cc2
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-03 11:59:57 -0300

    GET all news paginated

--- src/main/java/com/claudionogueira/news/controllers/NewsController.java
+++ src/main/java/com/claudionogueira/news/controllers/NewsController.java
@@ -1,8 +1,12 @@
 package com.claudionogueira.news.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.claudionogueira.news.models.News;
 import com.claudionogueira.news.services.NewsService;
 
 @RestController
@@ -13,5 +17,10 @@ public class NewsController {
 
 	public NewsController(NewsService service) {
 		this.service = service;
+	}
+
+	@GetMapping
+	public Page<News> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 }
--- src/main/java/com/claudionogueira/news/services/NewsService.java
+++ src/main/java/com/claudionogueira/news/services/NewsService.java
@@ -1,7 +1,10 @@
 package com.claudionogueira.news.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.claudionogueira.news.models.News;
 import com.claudionogueira.news.repositories.NewsRepo;
 import com.claudionogueira.news.services.interfaces.INewsService;
 
@@ -12,5 +15,10 @@ public class NewsService implements INewsService {
 
 	public NewsService(NewsRepo repo) {
 		this.repo = repo;
+	}
+
+	@Override
+	public Page<News> findAll(Pageable pageable) {
+		return repo.findAll(pageable);
 	}
 }
