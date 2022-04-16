68b59c4617b0ed96aa420e44d3197e5eba2977f5
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-16 12:24:28 -0300

    Add news view controller

---
+++ src/main/java/com/claudionogueira/news/views/NewsView.java
@@ -0,0 +1,28 @@
+package com.claudionogueira.news.views;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.claudionogueira.news.dto.NewsDTO;
+import com.claudionogueira.news.services.NewsService;
+
+@Controller
+public class NewsView {
+
+	private final NewsService service;
+
+	public NewsView(NewsService service) {
+		this.service = service;
+	}
+	
+	@GetMapping(value = "/news")
+	public ModelAndView getNews(Pageable pageable) {
+		ModelAndView mv = new ModelAndView("news-index");
+		Page<NewsDTO> news = service.findAll(pageable);
+		mv.addObject("allNews", news);
+		return mv;
+	}
+}
