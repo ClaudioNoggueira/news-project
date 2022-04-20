978177fc28d2a9e2c48b6b54f4b5b00efc78c8ef
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-20 11:23:56 -0300

    Implement get news details

--- src/main/java/com/claudionogueira/news/views/NewsView.java
+++ src/main/java/com/claudionogueira/news/views/NewsView.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.claudionogueira.news.dto.NewsDTO;
+import com.claudionogueira.news.exceptions.ObjectNotFoundException;
 import com.claudionogueira.news.services.NewsService;
 
 @Controller
@@ -23,10 +25,23 @@ public class NewsView {
 		return "redirect:/news";
 	}
 
+	// GET ALL NEWS
 	@GetMapping(value = "/news")
 	public String getNews(Pageable pageable, Model model) {
 		Page<NewsDTO> news = service.findAll(pageable);
 		model.addAttribute("allNews", news);
 		return "news/all-news";
+	}
+	
+	// GET NEWS DETAILS
+	@GetMapping(value = "/news/news-details/{id}")
+	public String getDetailsNews(@PathVariable String id, Model model) {
+		try {
+			service.findByIdDTO(id);
+		}catch(ObjectNotFoundException e) {
+			return "redirect:/news";
+		}
+		model.addAttribute("news", service.findByIdDTO(id));
+		return "news/details-news";
 	}
 }
