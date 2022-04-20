62391244f2ae97245be62eaf915a33421a0acef5
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-04-20 10:05:07 -0300

    Add details-category page

--- src/main/java/com/claudionogueira/news/views/CategoryView.java
+++ src/main/java/com/claudionogueira/news/views/CategoryView.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.claudionogueira.news.dto.CategoryDTO;
 import com.claudionogueira.news.exceptions.BadRequestException;
+import com.claudionogueira.news.exceptions.ObjectNotFoundException;
 import com.claudionogueira.news.services.CategoryService;
 
 @Controller
@@ -44,5 +46,16 @@ public class CategoryView {
 			return "redirect:/categories/add-category";
 		}
 		return "redirect:/categories";
+	}
+
+	@GetMapping(value = "/categories/category-details/{id}")
+	public String getDetailsCategory(@PathVariable String id, Model model) {
+		try {
+			service.findByIdDTO(id);
+		} catch (ObjectNotFoundException e) {
+			return "redirect:/categories";
+		}
+		model.addAttribute("category", service.findByIdDTO(id));
+		return "categories/details-category";
 	}
 }
