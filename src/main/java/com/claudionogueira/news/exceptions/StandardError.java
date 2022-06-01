10f49f6c36d4286edbba644c8329eeaa97b594be
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-01 06:50:57 -0300

    Refactor StandardError: Add list of Field and change timestamp to OffsetDateTime

--- src/main/java/com/claudionogueira/news/exceptions/StandardError.java
+++ src/main/java/com/claudionogueira/news/exceptions/StandardError.java
@@ -1,21 +1,31 @@
 package com.claudionogueira.news.exceptions;
 
 import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_EMPTY)
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long timestamp;
+	private OffsetDateTime timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
 
+	private List<Field> fields = new ArrayList<>();
+
 	public StandardError() {
 
 	}
 
-	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+	public StandardError(OffsetDateTime timestamp, Integer status, String error, String message, String path) {
+		super();
 		this.timestamp = timestamp;
 		this.status = status;
 		this.error = error;
@@ -23,11 +33,11 @@ public class StandardError implements Serializable {
 		this.path = path;
 	}
 
-	public Long getTimestamp() {
+	public OffsetDateTime getTimestamp() {
 		return timestamp;
 	}
 
-	public void setTimestamp(Long timestamp) {
+	public void setTimestamp(OffsetDateTime timestamp) {
 		this.timestamp = timestamp;
 	}
 
@@ -39,14 +49,6 @@ public class StandardError implements Serializable {
 		this.status = status;
 	}
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
 	public String getError() {
 		return error;
 	}
@@ -55,11 +57,62 @@ public class StandardError implements Serializable {
 		this.error = error;
 	}
 
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	public String getPath() {
 		return path;
 	}
 
 	public void setPath(String path) {
 		this.path = path;
+	}
+
+	public List<Field> getFields() {
+		return fields;
+	}
+
+	public void addField(String name, String message) {
+		this.getFields().add(new Field(name, message));
+	}
+
+	public void removeField(String name, String message) {
+		this.getFields().remove(new Field(name, message));
+	}
+
+	public static class Field {
+		private String name;
+		private String message;
+
+		public Field() {
+
+		}
+
+		public Field(String name, String message) {
+			super();
+			this.name = name;
+			this.message = message;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
 	}
 }
