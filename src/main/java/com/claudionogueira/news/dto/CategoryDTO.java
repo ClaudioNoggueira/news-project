2917ee407a0acb3de1067bd3c838943a7f8abb65
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-01 10:33:44 -0300

    Refactor CategoryDTO: Change set to set of static class News

--- src/main/java/com/claudionogueira/news/dto/CategoryDTO.java
+++ src/main/java/com/claudionogueira/news/dto/CategoryDTO.java
@@ -1,18 +1,23 @@
 package com.claudionogueira.news.dto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.claudionogueira.news.models.Category;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@JsonInclude(Include.NON_EMPTY)
 public class CategoryDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
 
-	private Set<NewsNoCategoryDTO> news = new HashSet<>();
+	private Set<CategoryDTO.News> news = new HashSet<>();
 
 	public CategoryDTO() {
 
@@ -44,7 +49,68 @@ public class CategoryDTO implements Serializable {
 		this.name = name;
 	}
 
-	public Set<NewsNoCategoryDTO> getNews() {
+	public Set<CategoryDTO.News> getNews() {
 		return news;
+	}
+
+	public void addNews(String title, String content, LocalDate date, AuthorDTO author) {
+		this.getNews().add(new News(title, content, date, author));
+	}
+
+	public void removeNews(String title, String content, LocalDate date, AuthorDTO author) {
+		this.getNews().remove(new News(title, content, date, author));
+	}
+
+	public static class News {
+		private String title;
+		private String content;
+
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+		private LocalDate date;
+		private AuthorDTO author;
+
+		public News() {
+
+		}
+
+		public News(String title, String content, LocalDate date, AuthorDTO author) {
+			super();
+			this.title = title;
+			this.content = content;
+			this.date = date;
+			this.author = author;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public LocalDate getDate() {
+			return date;
+		}
+
+		public void setDate(LocalDate date) {
+			this.date = date;
+		}
+
+		public AuthorDTO getAuthor() {
+			return author;
+		}
+
+		public void setAuthor(AuthorDTO author) {
+			this.author = author;
+		}
 	}
 }
